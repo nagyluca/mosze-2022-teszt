68d82c2636d17def773715f35bc5688ae2c51acd
Luca Nagy <nluca13579@gmail.com>
2022-09-20 00:27:57 +0200

    javitott verzioja a kodnak

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'  // ' helytt " hasznalata + << std::endl;
-    for (int i = 0;)  // nem fejezi be a for ciklust, 0-rol indul 1 helyett
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+        for (int i = 1;i<N_ELEMENTS+1;i++)
+        {
+            b[i-1] = i * 2;
+        }
+    for (int i = 0; i<=N_ELEMENTS-1; i++)
     {
-        b[i] = i * 2;  //b[i-1] = i * 2;
+        std::cout << "Ertek:" << b[i] << std::endl;
     }
-    for (int i = 0; i; i++)  // hianyzik meddig tart a for ciklus
-    {
-        std::cout << "Ertek:"  // nem iratja ki az erteket
-    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;  // nem nullazza ki az atlagot
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag=0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
